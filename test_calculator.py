4916e5a940ca6f300a821a2f959bb143f4517ee4
AmberNguyen249 <ambernguyen2017@gmail.com>
2025-04-10 15:26:11 -0400

    m

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,13 @@ import unittest
 from calculator import *
 
 
+def multiply(param, param1):
+    pass
+
+
+def divide(param, param1):
+    pass
+
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
@@ -15,12 +22,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    class TestMathFunctions(unittest.TestCase):
+
+
+        def test_multiply(self):  # 3 assertions
+            self.assertEqual(multiply(3, 4), 12)
+            self.assertEqual(multiply(0, 100), 0)
+            self.assertEqual(multiply(-2, 5), -10)
+
+        def test_divide(self):  # 3 assertions
+            self.assertEqual(divide(2, 10), 5)  # 10 / 2
+            self.assertEqual(divide(5, 0), 0)  # 0 / 5
+            with self.assertRaises(ZeroDivisionError):
+                divide(0, 10)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -43,7 +58,7 @@ class TestCalculator(unittest.TestCase):
 
         def test_log_invalid_argument(self):
             with self.assertRaises(ValueError):
-                logarithm(0, 5)  # Invalid base
+                logarithm(0, 5)
 
         def test_hypotenuse(self):
             self.assertEqual(hypotenuse(3, 4), 5)
@@ -53,7 +68,7 @@ class TestCalculator(unittest.TestCase):
 
         def test_sqrt(self):
             with self.assertRaises(ValueError):
-                square_root(-4)  # Invalid input
+                square_root(-4)
 
             self.assertEqual(square_root(0), 0)
             from math import isclose
